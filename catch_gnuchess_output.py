254005a516a91f2cf8a29823aac1bf996252e904
jeffthemaximum <frey.maxim@gmail.com>
2015-07-29 16:46:50 -0400

    reorganized project

---
+++ catch_gnuchess_output.py
@@ -0,0 +1,29 @@
+import subprocess
+
+proc = subprocess.Popen(['/usr/local/bin/gnuchessx'],
+						stdin=subprocess.PIPE,
+						stdout=subprocess.PIPE)
+
+#setup empty list to capture usr and cpu moves
+cpu_moves = []
+usr_moves = []
+
+while True:
+	#ask usr for move
+	inp = raw_input("Whatchur moov?") + "\n"
+
+	#add usr move to list
+	usr_moves.append(inp[:4])
+
+	#send usr move to gnuchess via subprocess
+	proc.stdin.write(inp)
+	proc.stdin.flush()
+
+	#receive output from gnuchess and print to console
+	for i in range(0,5):
+		line = proc.stdout.readline().rstrip()
+
+		#append cpu move to list
+		if i == 4:
+			cpu_moves.append(line[-4:])
+	print "cpu: ", repr(cpu_moves)
---
+++ string_split.py
@@ -0,0 +1,8 @@
+import pudb
+
+line = "e2e4"
+
+line = list(line)
+line.insert(2, "-")
+line = "".join(line)
+print line
